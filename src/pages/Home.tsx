e988f691ffce4efad68a6c708cc6fd965216fb04
Azmi117 <kennyhoii665@gmail.com>
2026-05-21 23:45:05 +0700

    FE-MoneySaver :rocket:

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return(
         <>
-            <h1 className="text-blue-500">Hello</h1>
+            <h1 className="text-red-500">Hello</h1>
         </>    
     )
 }
